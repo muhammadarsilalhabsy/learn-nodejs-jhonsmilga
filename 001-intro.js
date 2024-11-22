7a6b5471cd2520a139d8667ac4a2fffe7429c4ff
cubamas <cutibahteramas@gmail.com>
2024-11-23 04:53:03 +0800

    intro and global

---
+++ 001-intro.js
@@ -0,0 +1,9 @@
+const nilai = 10;
+
+if (nilai < 10) {
+  console.log("nilai lebih kecil");
+} else {
+  console.log("nilai lebih besar");
+}
+
+console.log(`this is my first node app!!!`);
---
+++ 002-global.js
@@ -0,0 +1,14 @@
+// Globals    - No Window echo !
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJs)
+// module     - info about current module (file)
+// process    - info about env where the program is beig executed
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process);
+console.log();
